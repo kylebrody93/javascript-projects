ee7ebc789a5bd30dca3e3db5b5d3d94605b5d2a3
kylebrody93 <41490305+kylebrody93@users.noreply.github.com>
2018-09-27 15:39:56 -0700

    Create paint.js
    This is a paint program I made in javascript using the p5js library.

---
+++ paint.js
@@ -0,0 +1,55 @@
+var red;
+var green;
+var blue;
+
+var posX;
+var posY;
+
+function setup() {
+	createCanvas(999, 500);
+  background(50, 150, 200,);
+  
+  red = 255;
+  green = 0;
+  blue = 0;
+  
+  posX = 1;
+  posY = 1;
+}
+
+
+function draw() {
+ 
+  fill(red, green, blue, 90);
+
+  
+  rect(posX, posY, 10, 10, 25);
+  
+}
+
+function keyPressed() {
+  if (red === 255) {
+    red = 0;
+    green = 255;
+    blue = 0;
+  } else if (green === 255){
+    red = 0;
+    green = 0;
+    blue = 255;
+  }
+  	else if (blue === 255){
+    red = 255;
+    green = 0;
+    blue = 0;
+  }
+}
+
+function mouseClicked(){
+  posX = mouseX;
+  posY = mouseY;
+}
+
+function mouseDragged(){
+  posX = mouseX;
+  posY = mouseY;
+}
