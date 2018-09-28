aeefe96786d8d98a84f8b87cb87b1823311b10ab
kylebrody93 <41490305+kylebrody93@users.noreply.github.com>
2018-09-27 22:49:35 -0700

    Update paint.js

--- paint.js
+++ paint.js
@@ -11,40 +11,38 @@ var sizeH;
 
 function setup() {
   createCanvas(999, 500);
-  background(50, 150, 200,);
-  
-  red = 255;
-  green = 0;
-  blue = 0;
-  
-  posX = 1;
-  posY = 1;
-  
+  background(185, 255, 255);
+
+  red = 50;
+  green = 150;
+  blue = 200;
+//brush starts off screen
+  posX = -11;
+  posY = 0;
+//starting cursor size
   sizeW = 10;
   sizeH = 10;
 }
 
 
 function draw() {
- 
+
   fill(red, green, blue, 90);
 
-  
   rect(posX, posY, sizeW, sizeH, 25);
-  
+
 }
 
 function keyPressed() {
   //user picks between 2 brush sizes and between random rgb colors
-  if (keyCode === ALT){
+  if (keyCode === ALT) {
     sizeW = 20;
     sizeH = 20;
-  }
-  else if (keyCode === CONTROL){
+  } else if (keyCode === CONTROL) {
     sizeW = 10;
     sizeH = 10;
-  }
-  else{
+  } 
+   	else {
     red = random(255);
     green = random(255);
     blue = random(255);
@@ -52,12 +50,14 @@ function keyPressed() {
 
 }
 
-function mouseClicked(){
+
+
+function mouseClicked() {
   posX = mouseX;
   posY = mouseY;
 }
 
-function mouseDragged(){
+function mouseDragged() {
   posX = mouseX;
   posY = mouseY;
 }
